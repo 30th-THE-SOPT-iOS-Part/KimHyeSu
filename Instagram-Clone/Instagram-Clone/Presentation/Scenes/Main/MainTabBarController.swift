3ddcd35d3b378d4d9dea2c82b62ff9b8535cabf1
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-04-22 17:04:19 +0900

    feat: #4 화면전환

---
+++ Instagram-Clone/Instagram-Clone/Presentation/Scenes/Main/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  Instagram-Clone
+//
+//  Created by 김혜수 on 2022/04/22.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

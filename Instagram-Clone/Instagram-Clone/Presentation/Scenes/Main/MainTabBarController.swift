9082391a7622f65393f950220c6401be24cb964e
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-04-22 17:50:25 +0900

    feat: #4 탭바 색상 전환

--- Instagram-Clone/Instagram-Clone/Presentation/Scenes/Main/MainTabBarController.swift
+++ Instagram-Clone/Instagram-Clone/Presentation/Scenes/Main/MainTabBarController.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 final class MainTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDelegate()
+        setUI()
+    }
+    
+    func setDelegate() {
+        self.delegate = self
+    }
+    
+    func setUI() {
+        self.tabBar.unselectedItemTintColor = .black
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    
+    private func isReelsViewController(index: Int) -> Bool {
+        return index == 2
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        let state = isReelsViewController(index: tabBarController.selectedIndex)
+        self.tabBar.backgroundColor = state ? .black : .white
+        self.tabBar.tintColor = state ? .white : .black
+        self.tabBar.unselectedItemTintColor = state ? .lightGray : .black
     }
 }

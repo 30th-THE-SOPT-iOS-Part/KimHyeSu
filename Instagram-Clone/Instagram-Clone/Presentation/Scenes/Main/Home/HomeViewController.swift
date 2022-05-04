520c8ec9fde90292167e22d1730ac37956d06199
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-05-04 14:36:51 +0900

    add: #6 에셋추가

---
+++ Instagram-Clone/Instagram-Clone/Presentation/Scenes/Main/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Instagram-Clone
+//
+//  Created by 김혜수 on 2022/05/04.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

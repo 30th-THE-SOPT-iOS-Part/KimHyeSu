4826c8a874dfa5128902a0ce3a894eff41486372
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-05-15 13:55:45 +0900

    feat: #8 회원가입 구현

--- Instagram-Clone/Instagram-Clone/Presentation/Scenes/Auth/Signup/SignupPasswordViewController.swift
+++ Instagram-Clone/Instagram-Clone/Presentation/Scenes/Auth/Signup/SignupPasswordViewController.swift
@@ -43,6 +43,14 @@ final class SignupPasswordViewController: BaseViewController, AuthProtocol {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
+    private func goToAuthCompleteViewController() {
+        guard let authCompleteViewController = UIStoryboard(name: Const.Storyboard.AuthComplete, bundle: nil)
+                .instantiateViewController(withIdentifier: Const.ViewController.AuthCompleteViewController) as? AuthCompleteViewController else { return }
+        authCompleteViewController.modalPresentationStyle = .fullScreen
+        authCompleteViewController.name = name ?? ""
+        self.present(authCompleteViewController, animated: true, completion: nil)
+    }
+    
     // MARK: - IBAction
     
     @IBAction func backButtonDidTap(_ sender: Any) {
@@ -50,10 +58,34 @@ final class SignupPasswordViewController: BaseViewController, AuthProtocol {
     }
     
     @IBAction func nextButtonDidTap(_ sender: Any) {
-        guard let authCompleteViewController = UIStoryboard(name: Const.Storyboard.AuthComplete, bundle: nil)
-                .instantiateViewController(withIdentifier: Const.ViewController.AuthCompleteViewController) as? AuthCompleteViewController else { return }
-        authCompleteViewController.modalPresentationStyle = .fullScreen
-        authCompleteViewController.name = name ?? ""
-        self.present(authCompleteViewController, animated: true, completion: nil)
+        postSignup()
+    }
+}
+
+extension SignupPasswordViewController {
+    
+    func postSignup() {
+        guard let name = name,
+              let password = passwordTextField.text
+        else { return }
+        AuthService.shared.postSignup(user: AuthRequest(name: name,
+                                                        email: name,
+                                                        password: password)) { result in
+            switch result {
+            case .success:
+                self.makeOKAlert(title: "회원가입 성공",
+                                 message: "회원가입 성공했습니다.",
+                                 okAction: { _ in self.goToAuthCompleteViewController() },
+                                 completion: nil)
+            case .requestErr:
+                self.makeOKAlert(title: "회원가입 실패", message: "", okAction: nil, completion: nil)
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
     }
 }
